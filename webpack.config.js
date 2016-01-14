1c38c8c493697fcce6f08a4d6f5be70e666cef8e
Shane Hsi <shane13hsi@gmail.com>
2016-01-14 19:05:45 +0800

    Add Html plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,6 @@
 var webpack = require('webpack');
-var HtmlWebpackPlugin = require('html-webpack-plugin');
-var H = require('./webpack.helper.js');
+var H = require('./webpack/webpack-helper.js');
+var HtmlPlugin = require('./webpack/html-plugin.js');
 
 var path = require('path');
 
@@ -25,5 +25,6 @@ module.exports = {
   },
 
   plugins: [
+    new HtmlPlugin()
   ]
 };
--- webpack/webpack-helper.js
+++ webpack/webpack-helper.js
@@ -1,19 +1,19 @@
 module.exports = {
-  'babel': function () {
-    return { test: /\.jsx?$/, loader: 'babel', exclude: /node_modules/ };
+  'babel': function() {
+    return {test: /\.jsx?$/, loader: 'babel', exclude: /node_modules/};
   },
-  'css-modules': function (config) {
+  'css-modules': function(config) {
     return {
       test: /\.css$/,
       include: config.include,
       loader: 'style!css?modules&importLoaders=1&localIdentName=[name]__[local]___[hash:base64:5]!postcss'
     }
   },
-  'css': function (config) {
+  'css': function(config) {
     return {
       test: /\.css$/,
       include: config.include,
       loader: 'style!css!postcss'
     }
   }
-}
+};
