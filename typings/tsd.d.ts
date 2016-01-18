bd2dc6cb93e3c22115b7a6ee5588b0147780c908
Shane Hsi <shane13hsi@gmail.com>
2016-01-18 11:26:27 +0800

    install react-router

--- typings/tsd.d.ts
+++ typings/tsd.d.ts
@@ -1,3 +1,5 @@
 /// <reference path="react/react.d.ts" />
 /// <reference path="react/react-dom.d.ts" />
 /// <reference path="node/node.d.ts" />
+/// <reference path="react-router/history.d.ts" />
+/// <reference path="react-router/react-router.d.ts" />
